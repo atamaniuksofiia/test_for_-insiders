acf95b969d68e350f22d9e60eed0475dde5ad381
atamaniuksofiia <sofiia.atamaniuk@edvantis.com>
2025-04-11 19:22:07 +0300

    project init with index

--- test_for_insiders_1/vite.config.js
+++ test_for_insiders_1/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: 'test_for_-insiders',
   plugins: [react()],
-})
+  build: {
+    sourcemap: true,
+  }
+});
